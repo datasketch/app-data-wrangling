13a264128f1047e6131f0fd181b8749afd251fb4
brazadas <anaherand@hotmail.com>
2020-12-01 19:12:43 -0500

    filter ui new module

--- app_.R
+++ app_.R
@@ -34,22 +34,93 @@ server <- function(input, output, session) {
   
   # controls
   output$controls <- renderUI({
-    div(div(class = "style_section", "Select columns"),
-        select_moduleUI("sel0", in_table))
+    tagList(div(class = "style_section", "Select columns"),
+            select_moduleUI("wr0", in_table),
+            div(class = "style_section", "Filter by columns"),
+            filter_moduleUI("wr1", in_table))
   })
   
   # initial table
   in_table <- read_csv("data/sampleData/pr.csv")
+  in_table$Country[2] <- ""
   observe({
     # select_moduleServer("sel0", in_table)
     
     # print(select_moduleServer("sel0", in_table))
   })
   
+  
+  filter_moduleServer <- function (id, data, options_0 = NULL, options_1 = NULL) {
+    
+    moduleServer(id, function (input, output, session) {
+      d0 <- data
+      ns <- NS(id)
+      opt_0 <- c("fil_00", "fil_01")
+      opt_1 <- c("fil_10", "fil_11")
+      names(opt_0) <- options_0 %||% c("in", "not in")
+      names(opt_1) <- options_1 %||% c("Keep NA's", "Keep empty")
+      
+      observeEvent(input$filter_0,  {
+        nm <- last(input$filter_0)
+        vc <- data[[nm]]
+        hd <- guess_hdType(vc)
+        if (hd %in% c("Num", "Yea", "Dat")) {
+          if (hd == "Dat") {
+            vc <- as.Date(vc, "%Y-%m-%d")
+          }
+          in0 <- sliderInput(ns(paste0(nm, "-", "filter_1")),
+                             "",
+                             min = min(vc, na.rm = TRUE),
+                             max = max(vc, na.rm = TRUE),
+                             value = c(min(vc, na.rm = TRUE), max(vc, na.rm = TRUE)),
+                             timeFormat = "%Y-%m-%d")
+        } else {
+          in0 <- selectizeInput(ns(paste0(nm, "-", "filter_2")),
+                                "",
+                                unique(vc), 
+                                # unique(c("", na.omit(vc))), 
+                                multiple = TRUE,
+                                options = list("plugins" = list("remove_button")))
+        }
+        # choices_kp <- c()
+        # checkbox_na_empty <- "" 
+        
+        if (any(is.na(vc)) & any(!nzchar(vc))) {
+          opt_1 <- opt_1
+        } else if (any(is.na(vc))) {
+          opt_1 <- opt_1[1]
+        } else if (any(!nzchar(vc))) {
+          opt_1 <- opt_1[2] 
+        } else {
+          opt_1 <- NULL
+        }
+        in1 <- ""
+        if (!is.null(opt_1)) in1 <- checkboxGroupInput(ns(paste0(nm, "-", "filter_")), "", choices = opt_1, selected = opt_1)
+        # if (any(!nzchar(vctr))) choices_kp <- c(choices_kp, "keep_empty")
+        # if (!is.null(na_empty_names) & (length(na_empty_names) >= length(choices_kp)) & (length(choices_kp) > 0)) names(choices_kp) <- na_empty_names[seq_along(choices_kp)]
+        # if (length(choices_kp) > 0) checkbox_na_empty <- checkboxGroupInput(ns("keep_na_empty"), "", choices = choices_kp, selected = choices_kp)
+        dv <- div(#id = id, 
+          div(style = "display: flex; justify-content: space-between;",
+              div(style = "font-weight: 600; padding: 0 39px 0 0;", paste0(nm, ":")),
+              # div(style = "font-size: 17px; font-weight: 400; padding: 0 17px 14px 0;", paste0(col, ":")),
+              # div(style = "padding: 0 17px 14px 0;", class = "control-label", paste0(col, ":")),
+              radioButtons(ns(paste0(nm, "-", "filter_3")), "", choices = opt_0)),
+          in0,
+          in1)
+        insertUI("#filter_", "afterEnd", dv) 
+        
+        
+      })
+      d0
+    })
+    
+  }
+  
   # table after changes
   out_table <- reactive({
     t0 <- in_table
-    t0 <- select_moduleServer("sel0", t0)
+    t0 <- select_moduleServer("wr0", t0)
+    t0 <- filter_moduleServer("wr1", t0)
     t0
   })
   
--- global_.R
+++ global_.R
@@ -1,3 +1,9 @@
+# QS---
+# filter radio buttons ¿inline o block?
+# filter insert ui afterEnd (para que queden primero los últimos) ¿o no?
+
+# select
+
 select_moduleUI <- function (id, data, options_0 = NULL, label_1 = NULL, label_2 = NULL, options_2 = NULL, placeholder_3 = NULL) {
   
   ns <- NS(id)
@@ -6,14 +12,11 @@ select_moduleUI <- function (id, data, options_0 = NULL, label_1 = NULL, label_2
   names(opt_0) <- options_0 %||% c("select", "unselect")
   names(opt_2) <- options_2 %||% c("that contain", "that do not contain")
   placeholder_3 <- placeholder_3 %||% "Write text"
-  # tagList(radioButtons(ns("sel_un_select"), "", opt_0),
-  #         selectizeInput(ns("columns_select"), label_1, names(data), multiple = TRUE, options = list("plugins" = list("remove_button"))),
-  #         radioButtons(ns("con_not_select"), label_2, opt_2),
-  #         textInput(ns("text_select"), "", placeholder = placeholder_3))
   tagList(radioButtons(ns("select_0"), "", opt_0),
           selectizeInput(ns("select_1"), label_1, names(data), multiple = TRUE, options = list("plugins" = list("remove_button"))),
           radioButtons(ns("select_2"), label_2, opt_2),
           textInput(ns("select_3"), "", placeholder = placeholder_3))
+  
 }
 
 select_moduleServer <- function (id, data) {
@@ -39,3 +42,71 @@ select_moduleServer <- function (id, data) {
   })
   
 }
+
+
+# filter
+
+filter_moduleUI <- function (id, data) {
+  
+  ns <- NS(id)
+  div(id = "filter_", 
+      selectizeInput(ns("filter_0"), "", names(data), multiple = TRUE, options = list("plugins" = list("remove_button"))))
+  
+}
+
+
+filter_moduleServer <- function (id, data, options_0 = NULL, options_1 = NULL) {
+  
+  moduleServer(id, function (input, output, session) {
+    d0 <- data
+    ns <- NS(id)
+    opt_0 <- c("fil_00", "fil_01")
+    opt_1 <- c("fil_10", "fil_11")
+    names(opt_0) <- options_0 %||% c("in", "not in")
+    names(opt_1) <- options_1 %||% c("Keep NA's", "Keep empty")
+    
+    observeEvent(input$filter_0,  {
+      nm <- last(input$filter_0)
+      vc <- data[[nm]]
+      hd <- guess_hdType(vc)
+      if (hd %in% c("Num", "Yea", "Dat")) {
+        if (hd == "Dat") {
+          vc <- as.Date(vc, "%Y-%m-%d")
+        }
+        in0 <- sliderInput(ns(paste0(nm, "-", "filter_1")),
+                             "",
+                             min = min(vctr, na.rm = TRUE),
+                             max = max(vctr, na.rm = TRUE),
+                             value = c(min(vctr, na.rm = TRUE), max(vctr, na.rm = TRUE)),
+                             timeFormat = "%Y-%m-%d")
+      } else {
+        in0 <- selectizeInput(ns(paste0(nm, "-", "filter_2")),
+                                "",
+                                unique(c("", na.omit(vc))), 
+                                multiple = TRUE,
+                                options = list("plugins" = list("remove_button")))
+      }
+      # choices_kp <- c()
+      # checkbox_na_empty <- "" 
+      # if (any(is.na(vctr))) choices_kp <- c("keep_na")
+      # if (any(!nzchar(vctr))) choices_kp <- c(choices_kp, "keep_empty")
+      # if (!is.null(na_empty_names) & (length(na_empty_names) >= length(choices_kp)) & (length(choices_kp) > 0)) names(choices_kp) <- na_empty_names[seq_along(choices_kp)]
+      # if (length(choices_kp) > 0) checkbox_na_empty <- checkboxGroupInput(ns("keep_na_empty"), "", choices = choices_kp, selected = choices_kp)
+      div(#id = id, 
+          div(style = "display: flex; justify-content: space-between;",
+              div(style = "font-weight: 600; padding: 0 39px 0 0;", paste0(nm, ":")),
+              # div(style = "font-size: 17px; font-weight: 400; padding: 0 17px 14px 0;", paste0(col, ":")),
+              # div(style = "padding: 0 17px 14px 0;", class = "control-label", paste0(col, ":")),
+              radioButtons(ns(paste0(nm, "-", "filter_3")), "", choices = opt_0)),
+          input#,
+          # checkbox_na_empty
+          )
+      
+      
+    })
+    
+  })
+  
+}
+
+
